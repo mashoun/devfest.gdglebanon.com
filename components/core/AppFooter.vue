c5991d0b747b2d3786a955c5d87c240aa9789abd
mashoun <mashoun.me@gmail.com>
2024-09-19 14:00:58 +0300

    first commit

---
+++ components/core/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container fluid class="px-5 mt-8" style="background-color: #202124">
+    <CommonSpeakerSocialButton :socialLinks="mainData.communityLinks" :dark="true" class="footer-icons" />
+  </v-container>
+</template>
+
+<script setup>
+const { mainData } = useJSONData();
+</script>
+
+<style></style>
