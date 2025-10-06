22f2bb630e82f414e7bacc08d1ddc299ff597a09
abedattal98 <abed_attal@hotmail.com>
2025-10-06 15:10:25 +0200

    add logos

--- components/common/sponsorInfo.vue
+++ components/common/sponsorInfo.vue
@@ -9,22 +9,43 @@
         ><b>{{ item.category_name }}</b></v-col
       >
       <v-col
-        md="2"
+        md="3"
         cols="6"
-        sm="3"
+        sm="4"
         class="text-center"
         v-for="(sponsor, indexp) in item.sponsors"
         :key="indexp"
       >
-        <div
-          style="background-color: #F3F3F3; border-radius: 15px;border: 1.5px solid black;"
-          class="pa-5"
-        >
+        <div class="sponsor-card">
           <ClientOnly>
             <v-tooltip location="bottom" :key="indexp">
               <template v-slot:activator="{ props }">
-                <a aria-label="sponsor name" :href="sponsor.link" target="_blank" v-bind="props">
-                  <v-img alt="sponsor-logo" :src="'/img/sponsors/' + sponsor.logo"></v-img>
+                <a 
+                  aria-label="sponsor name" 
+                  :href="sponsor.link" 
+                  target="_blank" 
+                  v-bind="props"
+                  class="sponsor-link"
+                >
+                  <div class="sponsor-content">
+                    <div class="logo-container">
+                      <v-img 
+                        alt="sponsor-logo" 
+                        :src="'/img/sponsors/' + sponsor.logo"
+                        class="sponsor-logo"
+                        contain
+                      ></v-img>
+                    </div>
+                    <div 
+                      v-if="sponsor.level && sponsor.level.trim() !== ''" 
+                      class="sponsor-level"
+                      :class="[
+                        item.category_name === 'Partners' ? 'level-partner' : 'level-' + (sponsor.level || '').toString().toLowerCase().trim()
+                      ]"
+                    >
+                      {{ sponsor.level }}
+                    </div>
+                  </div>
                 </a>
               </template>
               <span>{{ sponsor.name }}</span>
@@ -40,5 +61,95 @@
 const { sponsorsData } = useJSONData();
 </script>
 
-<style>
+<style scoped>
+.sponsor-card {
+  background-color: #F3F3F3;
+  border-radius: 15px;
+  border: 1.5px solid black;
+  min-height: 200px;
+  width: 100%;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.3s ease;
+  position: relative;
+}
+
+.sponsor-link {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  text-decoration: none;
+}
+
+.logo-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+}
+
+.sponsor-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+}
+
+.sponsor-logo {
+  max-height: 120px;
+  max-width: 100%;
+  width: auto;
+  height: auto;
+  margin: 0 auto;
+  object-fit: contain;
+}
+
+.sponsor-level {
+  margin-top: 10px;
+  padding: 4px 12px;
+  border-radius: 12px;
+  font-size: 0.8rem;
+  font-weight: bold;
+  text-transform: capitalize;
+  position: absolute;
+  bottom: 15px;
+  color: #000;
+}
+
+.sponsor-level.level-gold {
+  background-color: #ff9900;
+  color: #000;
+}
+
+.sponsor-level.level-silver {
+  background-color: #c0c0c0;
+  color: #000;
+}
+
+.sponsor-level.level-bronze {
+  background-color: #e06666;
+  color: #fff;
+}
+
+.sponsor-level.level-partner {
+  background-color: #4285f4;
+  color: #fff;
+}
+
+.sponsor-level.level-host {
+  background-color: #34a853;
+  color: #fff;
+}
+
+/* Default style for any undefined level */
+.sponsor-level:not(.level-gold):not(.level-silver):not(.level-bronze):not(.level-partner):not(.level-host) {
+  background-color: #34a853;
+  color: #fff;
+}
 </style>
